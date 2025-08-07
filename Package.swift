cd56c0df3a3b2af21ca67657d28414dfbd18343b
namsdk-ios-distribution <sodam.k@navercorp.com>
2025-08-07 13:54:58 +0900

    [NAMSDK] Update 8.7.0-test16 Version.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/robbiehanson/KissXML.git", exact: "5.3.3"),
-        //.package(url: "https://github.com/naver/nas-sdk-ios.git", exact: "1.1.2-test")
+        //.package(url: "https://github.com/naver/nas-sdk-ios.git", .upToNextMinor(from: "8.7.0-test16"))
         .package(url: "https://github.com/sodam-k/nas-sdk-ios.git", .upToNextMinor(from: "1.1.2-spm"))
     ],
     targets: [
